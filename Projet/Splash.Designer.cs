77b184d91c1032df0f19b97fbddf3ccb84b2b3f2
OUMAIMA MEZNI <oumaymamezni>
2022-02-22 00:37:22 +0100

    ffff

--- Projet/Splash.Designer.cs
+++ Projet/Splash.Designer.cs
@@ -32,6 +32,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.ProgressBar = new Guna.UI2.WinForms.Guna2CircleProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -65,12 +66,20 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_ticker);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 231);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
             // Splash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.ProgressBar);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -87,6 +96,7 @@
         private System.Windows.Forms.Label label1;
         private Guna.UI2.WinForms.Guna2CircleProgressBar ProgressBar;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
