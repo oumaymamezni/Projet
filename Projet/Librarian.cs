e03e197163c34a1f03bedfd8c59dfe5a501e33c9
OUMAIMA MEZNI <oumaymamezni>
2022-02-23 12:43:16 +0100

    base

--- Projet/Librarian.cs
+++ Projet/Librarian.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace Projet
 {
     public partial class Librarian : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-H8N9P9A\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True");
         public Librarian()
         {
             InitializeComponent();
@@ -27,6 +29,27 @@ namespace Projet
             MainForm m = new MainForm();
             m.Show();
             this.Hide();
+        }
+
+        private void Librarian_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(txtId.Text=="" || txtName.Text =="" || txtPassword.Text =="" || txtPhone.Text =="")
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into LibrarianTbl values("+ txtId.Text+",'"+txtName.Text+"','"+txtPassword.Text+"','"+txtPhone.Text+"')",con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Librarian added successfully");
+                con.Close();
+            }
         }
     }
 }
